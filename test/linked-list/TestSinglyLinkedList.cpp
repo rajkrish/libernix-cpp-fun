63668e26008f391124b4e0ad194517668bebfa30
Rajesh Krishnan <git@krishnan.cc>
2010-04-04 15:52:50 -0700

    Updated README and build.gmake.  Added BOOST_CHECK* calls in the testcases.

--- test/linked-list/TestSinglyLinkedList.cpp
+++ test/linked-list/TestSinglyLinkedList.cpp
@@ -6,6 +6,7 @@
 BOOST_AUTO_TEST_CASE(test_singly_linked_list_print_null)
 {
 	SllNode * pNode = NULL;
+	BOOST_CHECK_EQUAL(pNode, (SllNode*) NULL);
 	sllPrint(pNode, "NODE: ", "\n\n", FALSE);
 }
 
@@ -13,6 +14,7 @@ BOOST_AUTO_TEST_CASE(test_singly_linked_list_print_null)
 BOOST_AUTO_TEST_CASE(test_singly_linked_list_print_node1)
 {
 	SllNode *pNode = newNode(3);
+	BOOST_CHECK_MESSAGE(pNode != (SllNode *) NULL, "The pointer pNode should not be NULL");
 	sllPrint(pNode, "NODE: ", "\n\n", TRUE);
 }
 
